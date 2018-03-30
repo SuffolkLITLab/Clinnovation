5d2abfec271bb6934a2a56da49177b22b5b6d143
David Colarusso <dcolarusso@adm.suffolk.edu>
2018-03-30 11:30:55 -0400

    msg and icon updates

--- docs/2018/js/scripts.js
+++ docs/2018/js/scripts.js
@@ -1,2 +1,5 @@
-$('#msg').html("<a href=\"https://portalpro.suffolk.edu/forms3/Forms?v:=439\" style=\"color:black\">On-line Registration</a> $25 (public service waivers available). Join our <a href=\"http://suffolklitlab.org/slack\" style=\"color:black\">Slack team</a>.");
-$('#msg').show();
+function msg() {
+  $('#msg').html("<a href=\"https://portalpro.suffolk.edu/forms3/Forms?v:=439\" style=\"color:black\">On-line Registration</a> $25 (public service waivers available). Join our <a href=\"http://suffolklitlab.org/slack\" style=\"color:black\">Slack team</a>.");
+  $('#msg').show();
+  return true;
+}
