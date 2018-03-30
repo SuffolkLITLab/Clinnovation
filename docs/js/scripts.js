0ec9d2e2ced89e6aa5728930cb8d2b31007dbefc
David Colarusso <dcolarusso@adm.suffolk.edu>
2018-03-30 11:14:08 -0400

    js test

---
+++ docs/js/scripts.js
@@ -0,0 +1,2 @@
+$('#msg').html("<a href=\"https://portalpro.suffolk.edu/forms3/Forms?v:=439\" style=\"color:black\">On-line Registration</a> $25 (public service waivers available). Join our <a href=\"http://suffolklitlab.org/slack\" style=\"color:black\">Slack team</a>.");
+$('#msg').show();
