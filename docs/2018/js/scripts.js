808b876b2848e9c1a5a5fc74131d09a8803b4570
David Colarusso <dcolarusso@adm.suffolk.edu>
2018-04-03 11:06:02 -0400

    updates

--- docs/2018/js/scripts.js
+++ docs/2018/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("<a href=\"https://portalpro.suffolk.edu/forms3/Forms?v:=439\" style=\"color:black\">On-line Registration</a> $25 (public service waivers available). Join our <a href=\"http://suffolklitlab.org/slack\" style=\"color:black\">Slack team</a>.");
+  $('#msg').html("Join the conversation. Join our <a href=\"http://suffolklitlab.org/slack\" style=\"color:black\">Slack team</a> and say \"Hi\" over in #Clinnovation_2018.");
   $('#msg').show();
   return true;
 }
