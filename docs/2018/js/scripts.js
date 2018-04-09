abdb3ba003baa657f6e5e69b7edd2cfa8747d79d
David Colarusso <davidcolarusso@Liz-3.local>
2018-04-08 22:25:45 -0400

    updates

--- docs/2018/js/scripts.js
+++ docs/2018/js/scripts.js
@@ -1,5 +1,5 @@
 function msg() {
-  $('#msg').html("Join the conversation. Join our <a href=\"http://suffolklitlab.org/slack\" style=\"color:black\">Slack team</a> and say \"Hi\" over in #Clinnovation_2018.");
+  $('#msg').html("Join the conversation. Join our <a href=\"http://suffolklitlab.org/slack\" style=\"color:black\">Slack team</a>, and say \"Hi\" over in #Clinnovation_2018.");
   $('#msg').show();
   return true;
 }
